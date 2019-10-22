96cab49ef7fb415e829afb6c46c2115bfbb4a8ba
Martyn King <martyn@Martyns-MacBook-Pro.local>
2019-10-22 18:10:42 +0100

    Serialisers Migrations and Views

--- src/todo/migrations/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from todo.models import TodoItem
-
-class TodoItemSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializer.ReadOnlyField()
-    class Meta:
-    model = TodoItem
-    fields = ('url', 'title', 'completed', 'order')
